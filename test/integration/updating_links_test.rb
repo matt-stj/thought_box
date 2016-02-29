7c32f37528d5791fa64b96de89a5d810654f4b00
Matt Stjernholm <thew88@gmail.com>
2016-02-29 11:42:14 -0700

    need to add login route before link integration testing

---
+++ test/integration/updating_links_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UpdatingLinksTest < ActionDispatch::IntegrationTest
+  test "registered user can update link status" do
+    skip
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,21 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+end
 
-  # Add more helper methods to be used by all tests here...
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  def teardown
+    reset_session!
+  end
+
+  def setup
+    user = User.create(email: "hi@hi.com", password: "hi")
+    user.links.create(title: "hi", link_url: "http://www.hi.com")
+  end
 end
