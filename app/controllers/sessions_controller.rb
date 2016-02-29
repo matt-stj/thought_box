eea955f09a0bb126c53f5767f4d8717cfdef10df
Matt Stjernholm <thew88@gmail.com>
2016-02-29 09:31:00 -0700

    front end of user login form complete

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+    else
+      flash[:notice] = 'Invalid Login'
+      redirect_to login_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get '/', to: 'pages#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+
+  resources :users, only: [:new, :create]
+
 end
