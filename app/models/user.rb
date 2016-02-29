ce91ef49472f79740e325899706e309eaf245127
Matt Stjernholm <thew88@gmail.com>
2016-02-29 09:38:30 -0700

    add password confirmation to login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, uniqueness: true
+
+  validates :password, presence: true
+  validates :password, confirmation: true
+
+
 end
