3eb68fcfcfdafb12cf3ba1bdf6688bdaeba2683c
Matt Stjernholm <thew88@gmail.com>
2016-02-29 16:20:52 -0700

    button click registers on unread

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,29 @@
 //= require turbolinks
 //= require angular
 //= require_tree .
+
+var app = angular.module('Links', []);
+
+var apiURL = "/api/v1/links.json";
+
+function Link(id, link_url, title, read){
+  this.id = id
+  this.link_url = link_url
+  this.title = title
+  this.read = read
+}
+
+app.controller('LinksCtrl', function($scope, $http) {
+
+  $http.get(apiURL).success(function(response) {
+      $scope.links = response.map(function(link) {
+        return new Link(link.id, link.link_url, link.title, link.read)
+      });;
+    });
+
+    $scope.markUnread = function(link) {
+      console.log("hi")
+
+  };
+
+});
