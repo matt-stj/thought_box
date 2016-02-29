25c4b954a26560970f653fc5ed37dfd1b5d01db1
Matt Stjernholm <thew88@gmail.com>
2016-02-29 10:29:08 -0700

    require logged in user to view links page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :require_login
 
   helper_method :current_user
 
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+
+private
+
+  def require_login
+    unless current_user
+      redirect_to login_path
+    end
   end
 end
